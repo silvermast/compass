3ca995f59ae9ff35682b9ef228ba8d845b8c958f
Jason <jason@invexi.com>
2018-01-24 14:21:29 -0700

    added update uri, invoice

--- api/update.php
+++ api/update.php
@@ -7,7 +7,13 @@ use core\api\Response;
 
 $root = ROOT;
 
+ob_start();
+core\Update::init()->run();
+$update = ob_get_contents();
+ob_end_clean();
+
 Response::init([
     core\Shell::exec("git --exec-path='$root' fetch --quiet origin"),
     core\Shell::exec("git --exec-path='$root' reset --hard origin/master"),
+    $update,
 ], 200)->send();
